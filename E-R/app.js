d687456522222a454e11e54fa0bb6f47c35cb12f
iamteja9977 <thiru21ssc@gmail.com>
2022-09-23 10:56:43 +0530

    signup and storing data in DB

--- E-R/app.js
+++ E-R/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import config from "config";
 //apiRouter is the alias of the router
-import apiRouter from "./controllers/api/index.js";
+import apiRouter from "./controllers/api/index2.js";
 import taskRouter from "./controllers/tasks/index.js";
 const app = express();
 
@@ -9,7 +9,7 @@ const port = config.get("PORT");
 
 //APP LEVEL MIDDLE WARE
 app.use(express.json());
-
+import"./DBConnect.js";
 
 app.get("/", (req, res) => {
     res.status(200).json({ success: "HELLO FROM EXPRESS" });
@@ -18,20 +18,6 @@ app.get("/", (req, res) => {
 app.use("/api", apiRouter);
 app.use("/api/task", taskRouter);
 
-
-
-app.get("/use", (req, res, next) => {
-    try {
-        console.log("HELLO FROM USE");
-        console.log(req.payload);
-        console.log(req.adnan);
-        console.log(req.chetan);
-        res.status(200).json({ success: "Next Middleware" })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error " })
-    }
-})
 
 app.listen(port, () => {
     console.log("Server Started at Port : ", port);
