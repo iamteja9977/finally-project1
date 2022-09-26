f42213715de8cc2838fe8bf516dfb8ec9180ae9c
iamteja9977 <thiru21ssc@gmail.com>
2022-09-26 17:46:41 +0530

    in hotel we implemented payment router

--- mongoDB_app/app.js
+++ mongoDB_app/app.js
@@ -3,7 +3,8 @@ const app=express()
 const port=5000;
 
 import "./DBconnect.js";
-import hotelModel from "./modal/Hotels.js"
+import hotelModel from "./models/Hotels.js"
+import Payment from "./models/Payment.js"
 
 app.use(express.json())
 
@@ -27,6 +28,54 @@ app.post("/booking",async (req, res) => {
         await booking_data.save();
         // console.log(booking_data);
         res.status(200).json({ "success": "Route is Working" })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ "error": "Internal Server Error" })
+    }
+})
+
+
+
+app.post("/:user_id", async (req, res) => {
+    try {
+        let user_id = req.params.user_id;
+        // console.log(req.params);
+        const paymentData = await Payment.findOne({ user: user_id }).populate("user");
+        console.log(paymentData);
+
+
+        // paymentData.payments.push({
+        //     "payment_medium": "credit_card",
+        //     "payment_date": new Date("Mon Sep 26 2022 09:41:37 GMT+0530"),
+        //     "isSuccessful": true
+        // });
+        // console.log(paymentData.payments[0].payment_date);
+        // paymentData.save();
+
+        res.status(200).json({ "success": "Route is Working" })
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ "error": "Internal Server Error" })
+    }
+})
+
+
+app.post("/:user_id/:payment_id", async (req, res) => {
+    try {
+
+        // let user_id = req.params.user_id;
+        let payment_id = req.params.payment_id;
+
+        const filter = { _id: payment_id };
+
+        const update = { isSuccessful: false };
+
+        let paymentModified = await Payment.findOneAndUpdate(filter, update);
+
+        paymentModified.save();
+        res.status(200).json({ "success": "Route is Working" })
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ "error": "Internal Server Error" })
